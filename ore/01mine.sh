4d9d987550e4569f8394a219d7f33888f2e0243a
lautianv <85725581+lautianv@users.noreply.github.com>
2024-08-18 05:47:18 +0800

    Delete ore directory

--- ore/01mine.sh
+++
@@ -1,3 +0,0 @@
-./ore-miner  mine --address CJxoYG6mgmEoXB9V2Mjsckodw3vvcSka4KaJHF9XvNRG --threads 30 --invcode Q84M6R
-pause
-
--- ore/02mine.sh
+++
@@ -1,3 +0,0 @@
-./ore-miner  claim --address CJxoYG6mgmEoXB9V2Mjsckodw3vvcSka4KaJHF9XvNRG  --invcode Q84M6R
-pause
-
